1eb5e54354677345ec3cc1010cabe07716a68217
sanjaypdm2007-commits <sanjaypdm2007@gmail.com>
2026-04-11 00:04:07 +0530

    .env edit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
+console.log("CLIENT ID:", process.env.REACT_APP_GOOGLE_CLIENT_ID);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
